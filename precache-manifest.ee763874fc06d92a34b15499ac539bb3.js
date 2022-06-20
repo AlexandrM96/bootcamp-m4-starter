db54d96924a65f111bae442593f97d6d941e9ecb
AlexandrM96 <super.dwtd2013@yandex.ru>
2022-06-20 12:39:29 +0300

    Updates

---
+++ precache-manifest.ee763874fc06d92a34b15499ac539bb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bad03e5932004a0af0c98cbc2e74940",
+    "url": "/bootcamp-m4-starter/index.html"
+  },
+  {
+    "revision": "f50e686387fe65648854",
+    "url": "/bootcamp-m4-starter/static/css/main.84ac334f.chunk.css"
+  },
+  {
+    "revision": "8dc6517baa6054130d81",
+    "url": "/bootcamp-m4-starter/static/js/2.15b6790f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bootcamp-m4-starter/static/js/2.15b6790f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f50e686387fe65648854",
+    "url": "/bootcamp-m4-starter/static/js/main.ac523ba7.chunk.js"
+  },
+  {
+    "revision": "c4a5ab99ffa0fefeb696",
+    "url": "/bootcamp-m4-starter/static/js/runtime-main.abf562ea.js"
+  }
+]);
